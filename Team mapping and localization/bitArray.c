04c7c6a27d8e36b5e6ff06f369af5bf65c66bb1c
Mohamed Khaled <Mohamed_Khaled_Kamal@outlook.com>
2021-05-29 10:43:00 +0200

    The array is now 2d
    made the 1d array bbe accessed lika a 2d array bit by bit

--- Team mapping and localization/bitArray.c
+++ Team mapping and localization/bitArray.c
@@ -1,45 +1,47 @@
-#include <unistd.h>
+//#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include<stdint.h>
 
-uint8_t arr[2];
-uint8_t read_arr(uint8_t index)
+
+#define MAP_MAX_X 100
+#define MAP_MAX_Y 150
+uint8_t Map[1875];  // 100bit*150bit == 100cm*150cm
+
+// x is width MAP_MAX_X   is its size 
+//y is height MAP_MAY_Y   is its size
+// we access the array by coordinated(x,y)
+
+uint8_t read_Map(uint8_t index_x,uint8_t index_y) //function takes (x,y) and return the value of the bit wether it is 1 or 0
 {
+    // index= x+ width*y   source: https://softwareengineering.stackexchange.com/questions/212808/treating-a-1d-data-structure-as-2d-grid/212813
+    int index= index_x + MAP_MAX_X * index_y;
     uint8_t bigIndex=index/8;
     uint8_t shiftIndex=index%8;
-    return((arr[bigIndex]>>shiftIndex)&0x01);
+    return((Map[bigIndex]>>shiftIndex)&0x01);
 }
-void write_arr(uint8_t index,uint8_t state)
+void write_Map(uint8_t index_x,uint8_t index_y,uint8_t state)  // (x_position, y_postion, state to set the bit or clear it)
 {
+    int index = index_x + MAP_MAX_X * index_y;
     uint8_t bigIndex=index/8;
     uint8_t shiftIndex=index%8;
     if(state)    //set bit
     {
-        arr[bigIndex]|=1<<shiftIndex;
+        Map[bigIndex]|=1<<shiftIndex;
     }
     else    // clear bit 
     {
-        arr[bigIndex]&=~1<<shiftIndex;
+        Map[bigIndex]&=~1<<shiftIndex;
     }
 
 }
 int main(void)
 {
-    for(int i=0;i<4;i++)
-    {
-        write_arr(i,1);
-    }
-    for(int i=8;i<16;i++)
-    {
-        write_arr(i,1);
-
-    }
-    //write_arr(7,1);
-    for(int i=0;i<16;i++)
-    {
-        printf("inedx[%d]=%d\n",i,read_arr(i));
-    }
+    write_Map(10, 11, 1);
+    printf("%d\n", read_Map(10, 11));
+    printf("%d\n", read_Map(10, 10));
+    /*read_Map(10, 11);
+    read_Map(10, 10);*/
 }
 
 
